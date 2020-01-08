703ef36ac443ae66cbc79003642e79ebfac13b43
jiangkuangpu <jiangkuangpu@qianxin.com>
2020-01-08 17:34:56 +0800

    date -u +%Y_%m_%d_%H_%M

--- src/Go/kata/Josephus Permutation.go
+++ src/Go/kata/Josephus Permutation.go
@@ -2,12 +2,18 @@ package kata
 
 //5 https://www.codewars.com/kata/josephus-permutation/train/go
 func Josephus(items []interface{}, k int) []interface{} {
-
-	// Your code:
-	var res []interface{}
-	for i := 0; i*k < len(items); i++ {
-		res := append(res, items[i*k])
-		return res
+	//按题意需要处理空切片且返回的切片长度和容量需要都为空
+	res := []interface{}{}
+	ch := make(chan interface{}, len(items))
+	for _, v := range items {
+		ch <- v
 	}
-	return nil
+	for i := 1; len(ch) > 0; i++ {
+		if i % k == 0 {
+			res = append(res, <- ch)
+		} else {
+			ch <- <- ch
+		}
+	}
+	return res
 }
