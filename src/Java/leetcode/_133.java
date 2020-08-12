acf0abcd8eb62e376fed47342ec4423f1a92b18a
kp1nz <815153150@qq.com>
2020-08-12 22:46:29 +0800

    u

---
+++ src/Java/leetcode/_133.java
@@ -0,0 +1,63 @@
+package Java.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * dfs 复制图
+ * 遍历的时候用字典记录访问点
+ *
+ * @Author: kp1nz
+ * @Date: 2020/8/12 10:06 下午
+ */
+public class _133 {
+    HashMap<Integer, Node> dic = new HashMap<>();
+    HashMap<Integer, List<Node>> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        if (node.neighbors.size() == 0) return new Node(node.val, new ArrayList<>());
+        dfs(node);
+        for (int key : map.keySet()){
+            List<Node> nebs = map.get(key);
+            Node copy = dic.get(key);
+            for(Node a : nebs){
+                copy.neighbors.add(dic.get(a.val));
+            }
+        }
+        return dic.get(node.val);
+    }
+
+    private void dfs(Node node) {
+        for (Node n : node.neighbors) {
+            if (!map.containsKey(n.val)) {
+                Node temp = new Node(n.val, new ArrayList<>());
+                dic.put(n.val, temp);
+                map.put(n.val, n.neighbors);
+                dfs(n);
+            }
+        }
+    }
+
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
